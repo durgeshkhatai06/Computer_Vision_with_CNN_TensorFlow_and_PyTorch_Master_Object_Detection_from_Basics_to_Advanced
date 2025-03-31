414f3fcc35619c56ccb50f846d5be97e04afad3e
durgeshkhatai06 <nandandurgesh27@gmail.com>
2025-03-31 18:14:10 +0530

    Added some codes in an app.py in this project

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 import logging
 
+## logging setting
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    handlers=[
+        logging.FileHandler("app1.log"),
+        logging.StreamHandler()
+    ]
+)
